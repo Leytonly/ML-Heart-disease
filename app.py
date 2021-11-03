1c272e701071ff3155dcfbdc728011ee4a55db87
Leyton <leyton.ly@epitech.eu>
2021-11-03 21:44:21 +0100

    a

--- app.py
+++ app.py
@@ -38,14 +38,14 @@ def main():
     with col2:
         st.subheader(" Find out the most suitable crop to grow in your farm 👨‍🌾")
         Age = st.number_input("Age (0 - 99)", 1,99)
-        Sex = st.number_input("Sex (0 = Male, 1 = Female", 0,1)
+        P = st.number_input("Phosporus", 1,10000)
         K = st.number_input("Potassium", 1,10000)
         temp = st.number_input("Temperature",0.0,100000.0)
         humidity = st.number_input("Humidity in %", 0.0,100000.0)
         ph = st.number_input("Ph", 0.0,100000.0)
         rainfall = st.number_input("Rainfall in mm",0.0,100000.0)
 
-        feature_list = [Age, Sex, K, temp, humidity, ph, rainfall]
+        feature_list = [Age, P, K, temp, humidity, ph, rainfall]
         single_pred = np.array(feature_list).reshape(1,-1)
         
         if st.button('Predict'):
