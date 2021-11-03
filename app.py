83e15520a4b3a396f998ba6a7132a5478b39b931
Leyton <leyton.ly@epitech.eu>
2021-11-03 21:52:17 +0100

    a

--- app.py
+++ app.py
@@ -37,16 +37,21 @@ def main():
 
     with col2:
         st.subheader(" Find out the most suitable crop to grow in your farm 👨‍🌾")
-        Age = st.number_input("Age (0 - 99)", 0, 99, 0)
-        Sex = st.number_input("Sex", 0, 1, 0)
-        P = st.number_input("Phosporus", 1,10000)
-        K = st.number_input("Potassium", 1,10000)
-        temp = st.number_input("Temperature",0.0,100000.0)
-        humidity = st.number_input("Humidity in %", 0.0,100000.0)
-        ph = st.number_input("Ph", 0.0,100000.0)
-        rainfall = st.number_input("Rainfall in mm",0.0,100000.0)
+        Age = st.number_input("Age (0 - 99)", 0,99, 0)
+        Sex = st.number_input("Sex (0 = Male, 1 = Female", 0, 1, 0)
+        CPT = st.number_input("Chest Pain Type (0 = No pain, 3 = Tremendeous pain)", 0, 3, 0)
+        RBP = st.number_input("Resting Blood Pressure (94 = 200)", 94, 200, 94)
+        Chol = st.number_input("Cholesterol (126 = 564)", 126, 564, 126)
+        FBS = st.number_input("Fasting Blood Sugar (0 = false, 1 = true)", 0, 1, 0)
+        Restecg = st.number_input("Resting Electrocardiographic Results (0 = 2", 0, 2, 0)
+        Thalach = st.number_input("Maximum Heart Rate Achieved (71 = 202)", 71, 202, 71)
+        Exang = st.number_input("Exercise Induced Angina (0 = no, 1 = yes)", 0, 1, 0)
+        Oldpeak = st.number_input("ST depression induced by exercise relative to rest (0.0 - 6.2)", 0.0, 6.2, 0.0)
+        Slope = st.number_input("The slope of the peak exercise ST segment (0 - 2)", 0, 2, 0)
+        Ca = st.number_input("Number of major vessels (0 - 3)", 0, 3, 0)
+        Thal = st.number_input("3 = normal; 6 = fixed defect; 7 = reversable defect (0 - 7)", 0, 7, 0)
 
-        feature_list = [Age, Sex, P, K, temp, humidity, ph, rainfall]
+        feature_list = [Age, Sex, CPT, RBP, Chol, FBS, Restecg, Thalach, Exang, Oldpeak, Slope, Ca, Thal]
         single_pred = np.array(feature_list).reshape(1,-1)
         
         if st.button('Predict'):
